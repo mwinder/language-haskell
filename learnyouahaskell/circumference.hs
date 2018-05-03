322d5f08ad86bde02e6306a1157783894f8d90f6
Matthew Winder <mwinder@users.noreply.github.com>
2018-05-03 21:53:11 +0100

    Initial import

---
+++ learnyouahaskell/circumference.hs
@@ -0,0 +1,8 @@
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
+area :: Integer -> Integer -> Integer
+area w h = w * h
